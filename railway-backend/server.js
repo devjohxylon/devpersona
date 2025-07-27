48a1037fc7ab4eedd8b4ea73adbdb08bf59b8e61
Johxylon <captjohn.lohse@gmail.com>
2025-07-27 19:13:14 -0400

    Add CORS headers to allow frontend requests from devpersonality.com

--- railway-backend/server.js
+++ railway-backend/server.js
@@ -4,6 +4,20 @@ const { PrismaClient } = require('@prisma/client')
 const app = express()
 const prisma = new PrismaClient()
 
+// CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://www.devpersonality.com')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  res.header('Access-Control-Allow-Credentials', 'true')
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200)
+  } else {
+    next()
+  }
+})
+
 // Simple middleware
 app.use(express.json())
 
